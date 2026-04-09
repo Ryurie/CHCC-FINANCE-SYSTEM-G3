9f6b2640229e177a17776fcee5987a735f6e0d0c
Ryurie <rjaytzy123@gmail.com>
2026-04-09 19:09:29 +0800

    Delete finance-fee-system directory

--- finance-fee-system/READMI.md
+++
@@ -1,1 +0,0 @@
-Kain mo etits ko
--- finance-fee-system/api/fees/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-
-require_once '../../controllers/FeeController.php';
-
-$controller = new FeeController();
-$controller->getFees();
-?>
